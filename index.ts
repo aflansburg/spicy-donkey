6133c40518f4e9b19825a5d1faf616b5f041cf15
aflansburg <abeflansburg@gmail.com>
2024-07-31 16:44:11 -0500

    update index to support next paths

--- index.ts
+++ index.ts
@@ -12,15 +12,30 @@ function getRandomElement(arr: string[]): string {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+function isNextJsEnvironment(): boolean {
+  return (
+    process.env.NEXT_PUBLIC_ENV !== undefined ||
+    fs.existsSync(path.join(process.cwd(), 'next.config.js')) ||
+    fs.existsSync(path.join(process.cwd(), '.next'))
+  );
+}
+
 function generateRandomCombination(
   adjectiveType: AdjectiveType,
   nounType: NounType,
+  baseDir?: string,
 ): string {
+  if (!baseDir && isNextJsEnvironment()) {
+    baseDir = path.join(process.cwd(), 'public');
+  } else {
+    baseDir = baseDir || __dirname;
+  }
+
   const adjectives = loadJson(
-    path.join(__dirname, 'data', 'adjectives', `${adjectiveType}.json`),
+    path.join(baseDir, 'data', 'adjectives', `${adjectiveType}.json`),
   );
   const nouns = loadJson(
-    path.join(__dirname, 'data', 'nouns', `${nounType}s.json`),
+    path.join(baseDir, 'data', 'nouns', `${nounType}s.json`),
   );
 
   return `${getRandomElement(adjectives)} ${getRandomElement(nouns)}`;
